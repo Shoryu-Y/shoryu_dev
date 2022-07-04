e11d4dcdd0816e1dfc15b938c9942f1f38a4a248
Shoryu Yamamoto <shoryu13@gmail.com>
2022-07-05 02:10:11 +0900

    Revert "Merge branch 'main' of https://github.com/Shoryu-Y/shoryu_dev"
    This reverts commit 9b79ddddcd56d7dee07310cd6c35f957f73865ee, reversing
    changes made to 4537902bb7c501dffac4fd889ea55bafcba32d88.

---
+++ lib/amplifyconfiguration.dart
@@ -0,0 +1,78 @@
+const amplifyconfig = ''' {
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+                "CredentialsProvider": {
+                    "CognitoIdentity": {
+                        "Default": {
+                            "PoolId": "ap-northeast-1:3b60d9ea-242c-48bb-88e8-b9e5e8185e03",
+                            "Region": "ap-northeast-1"
+                        }
+                    }
+                },
+                "CognitoUserPool": {
+                    "Default": {
+                        "PoolId": "ap-northeast-1_QdBpOeMcP",
+                        "AppClientId": "45iuso21usg894b5lnn7r1mk8",
+                        "AppClientSecret": "1cp2s8gonsgifiirtpmfkk6q9dku8fmr4bnn3e8uf75fepk3icf0",
+                        "Region": "ap-northeast-1"
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "authenticationFlowType": "USER_SRP_AUTH",
+                        "socialProviders": [],
+                        "usernameAttributes": [
+                            "EMAIL"
+                        ],
+                        "signupAttributes": [
+                            "EMAIL"
+                        ],
+                        "passwordProtectionSettings": {
+                            "passwordPolicyMinLength": 8,
+                            "passwordPolicyCharacters": []
+                        },
+                        "mfaConfiguration": "OFF",
+                        "mfaTypes": [
+                            "SMS"
+                        ],
+                        "verificationMechanisms": [
+                            "EMAIL"
+                        ]
+                    }
+                },
+                "PinpointAnalytics": {
+                    "Default": {
+                        "AppId": "bffef174f0ab4f218a253991b9ac5c05",
+                        "Region": "us-west-2"
+                    }
+                },
+                "PinpointTargeting": {
+                    "Default": {
+                        "Region": "us-west-2"
+                    }
+                }
+            }
+        }
+    },
+    "analytics": {
+        "plugins": {
+            "awsPinpointAnalyticsPlugin": {
+                "pinpointAnalytics": {
+                    "appId": "bffef174f0ab4f218a253991b9ac5c05",
+                    "region": "us-west-2"
+                },
+                "pinpointTargeting": {
+                    "region": "us-west-2"
+                }
+            }
+        }
+    }
+}''';
